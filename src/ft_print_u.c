15e55efa10b8b6fcda6db5242ac75dbbae7b6e1d
Corry TERBLANCHE <cterblan@e5r11p8.wethinkcode.co.za>
2018-08-22 08:23:52 +0200

    added u handler & print

---
+++ src/ft_print_u.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_u.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/22 07:39:54 by cterblan          #+#    #+#             */
+/*   Updated: 2018/08/22 07:57:19 by cterblan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/ft_printf.h"
+
+void	ft_print_u(unsigned long long n, t_data *d)
+{
+	char	*new;
+
+	new = ft_ulltoa_base(n, 10);
+	d->rtn += ft_strlen(new);
+	ft_putstr(new);
+}
