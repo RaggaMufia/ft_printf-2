9a27084e0a7cf11c01179e0bf3e9ab5f31db7b92
Corry TERBLANCHE <cterblan@e5r11p8.wethinkcode.co.za>
2018-08-25 13:41:17 +0200

    added p flags

--- m.c
+++ m.c
@@ -6,7 +6,7 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/19 08:42:32 by cterblan          #+#    #+#             */
-/*   Updated: 2018/08/25 13:32:53 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/08/25 13:40:46 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int		main(void)
 
 	//ft_printf("%%%%%20d,%c,%o,%u,%lx,%p,%s,%D,%O,%X,%U,%C%S", l, c, i, i, ul, &c, s, l, l, ul, ul, wc, ws);
 	//printf("\n%%%%%20d,%c,%o,%u,%lx,%p,%s,%D,%O,%X,%U,%C%S", l, c, i, i, ul, &c, s, l, l, ul, ul, wc, ws);
-	mine  = ft_printf("%-020.12o|", i);
+	mine  = ft_printf("%020.12p|", 0);
 	ft_putchar('\n');
-	theirs = printf("%-020.12o|", i);
+	theirs = printf("%20.12p|", 0);
 	printf("\n%i\n%i", mine, theirs);
 }
--- src/ft_print_p.c
+++ src/ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: cterblan <cterblan@student.wethinkcode>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/22 07:39:54 by cterblan          #+#    #+#             */
-/*   Updated: 2018/08/22 12:14:36 by cterblan         ###   ########.fr       */
+/*   Updated: 2018/08/25 13:40:00 by cterblan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ void	ft_print_p(void *addr, t_data *d)
 	char	*new;
 
 	new = ft_ulltoa_base((long long)addr, 16);
+	ft_flag_size(ft_strlen(new), d);
+	d->fw -= 2;
+	ft_flag_width(d);
 	ft_putstr("0x");
-	d->rtn += 2;
-	d->rtn += ft_strlen(new);
+	d->rtn += ft_strlen(new) + 2;
+	ft_flag_pre(new, d);
 	ft_putstr(new);
+	ft_flag_width(d);
 	free(new);
 }
